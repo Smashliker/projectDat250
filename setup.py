b2dd4a4f618a9e6103d25f7323deb2cdba544161
oveuis <o.oftedal@stud.uis.no>
2020-09-20 19:26:28 +0200

    Set up setup.py, experimenting with templates

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='projectDat250',
+    packages=['projectDat250'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
