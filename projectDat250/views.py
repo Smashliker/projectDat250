4afeb07dc697718e3840acee71636228205a17f5
= <o.oftedal@stud.uis.no>
2020-09-22 11:41:21 +0200

    Fixed login screen linking and routing

--- projectDat250/views.py
+++ projectDat250/views.py
@@ -1,7 +1,17 @@
 from projectDat250 import app
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
