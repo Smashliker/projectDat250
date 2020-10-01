8846da124456f56b444b2c548c1b1737560141af
benjamin <bengame@hotmail.no>
2020-10-01 22:11:45 +0200

    Updated the database with more tests

--- projectDat250/schema.sql
+++ projectDat250/schema.sql
@@ -8,7 +8,8 @@ CREATE TABLE "users" (
 
 CREATE TABLE post (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER NOT NULL,
+  author_id TEXT NOT NULL,
+  author_name TEXT NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
@@ -16,10 +17,9 @@ CREATE TABLE post (
 );
 
 CREATE TABLE "friends" (
-  "number" INTEGER NOT NULL,
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "userid" TEXT NOT NULL,
-  "friendid" TEXT NOT NULL,
-  PRIMARY KEY("number")
+  "friendid" TEXT NOT NULL
 );
 
 INSERT INTO "users" 
@@ -32,5 +32,11 @@ INSERT INTO "users"
   ("userid","username", "password")
 VALUES('afhf', 'min bror', 'funny');
 
-INSERT INTO "friends" ("number","userid","friendid") VALUES(1,"djfnj", "asdasd");
-INSERT INTO "friends" ("number","userid","friendid") VALUES(2,"djfnj", "afhf");
+INSERT INTO "friends" ("userid","friendid") VALUES("djfnj", "asdasd");
+INSERT INTO "friends" ("userid","friendid") VALUES("djfnj", "afhf");
+
+INSERT INTO post (author_id,author_name,created,title,body) VALUES("asdasd","Sakurai",'2000-01-01 00:00:01',"lol",
+"funnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunnyfunny");
+
+INSERT INTO post (author_id,author_name,created,title,body) VALUES("afhf","min bror",'2020-10-01 16:02:13',"POGGERS",
+"minecraft Steve building");
