f0bdf81371f7dde6d2126f535c3f730d8c11c669
= <o.oftedal@stud.uis.no>
2020-10-01 14:35:11 +0200

    Working on secure login

--- projectDat250/__init__.py
+++ projectDat250/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 from . import db
 
 import projectDat250.views
 
 db.init_app(app)
+
+bcrypt = Bcrypt(app)
 
--- projectDat250/views.py
+++ projectDat250/views.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, redirect, url_for, request, session
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from . import db
+from . import models
 import os
 @app.route('/')
 def index():
@@ -13,6 +15,7 @@ app.secret_key = os.urandom(16)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    form = LoginForm
     if request.method == 'POST':
         session['username'] = request.form['username']
         session['password'] = request.form['password']
@@ -34,3 +37,43 @@ def logout():
 @app.route('/aboutUs')
 def aboutUs():
         return render_template('aboutUs.html')
+
+@app.route('/createUser', methods=['GET', 'POST'])
+def createUser():
+    if request.method == 'POST':
+        if validateUsername(request.form['username']) is True and len(request.form['password']) >= 1:
+            #TODO: Actually create user object from class in db.py
+            hashed = bcrypt.generate_password_hash(request.form['password'])
+            user = User(username=request.form['username'], password=hashed, userid = generateUserID)
+            db,session.add(user)
+            db.session.commit()
+            return redirect(url_for('index'))
+        return '''
+            <form method="post">
+                <p><input type="text" name=username placeholder="Username">
+                <p><input type="password" name=password placeholder="Password">
+                <p><input type="password" name=password placeholder="Repeat Password">
+                <p><input type="submit" value="Create User">
+            '''
+    else:
+        return redirect(url_for('index'))
+    
+def validateUsername(wantedName):
+    validated = True
+    for username in db.query_db('select username from users'):
+        if wantedName == username:
+            validated = False
+            break
+    return validated
+    
+def generateUserID():
+    while True:
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+
+        for userid in db.query_db('select userid from users'):
+            if result_str == userid:
+                break
+        return result_str 
+                
+        
