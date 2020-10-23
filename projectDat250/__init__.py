cc35d32e2832a5ee1b519f7695cc29a0e3bcd3b0
= <o.oftedal@stud.uis.no>
2020-10-24 00:35:25 +0200

    Trying to fix CSRF token

--- projectDat250/__init__.py
+++ projectDat250/__init__.py
@@ -20,7 +20,8 @@ DATABASE = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 #app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=20)
 # Set the secret key to some random bytes
-app.secret_key = os.urandom(16)
+#app.secret_key = os.urandom(16)
+app.config['SECRET_KEY'] = os.urandom(16)
 
 from flask_wtf.csrf import CSRFProtect
 
