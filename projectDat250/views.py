854dafe80a5a027ff0523e8c9b6a15666509c44a
= <o.oftedal@stud.uis.no>
2020-10-24 00:19:16 +0200

    debugging...

--- projectDat250/views.py
+++ projectDat250/views.py
@@ -109,9 +109,7 @@ def login():
     #Create a WTForm for login
     print("new request")
     form = LoginForm()
-    print(request.form["username"])
-    print(request.form["password"])
-    print(forms.errors)
+    print(form.errors)
     if form.validate_on_submit():
         print("Form was validated")
         #Check for the username in the database to find a valid user
