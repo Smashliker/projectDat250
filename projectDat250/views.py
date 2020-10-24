5718c525f32c22d0d65116264de0c55973005aeb
= <o.oftedal@stud.uis.no>
2020-10-24 20:08:12 +0200

    Fixing post bug

--- projectDat250/views.py
+++ projectDat250/views.py
@@ -260,6 +260,7 @@ def createPost():
 @login_required
 def viewPosts(post_id):
     posts = Post.query.all()
+    print("post id:")
     print(posts[0].id)
     post = Post.query.filter_by(id=post_id).first()
     comments = Comments.query.filter_by(post_id=post_id).all()
