925f2fba9974394a3a542cfad34b09045e7b5e59
benjamin <bengame@hotmail.no>
2020-10-24 22:27:12 +0200

    Added admin-insertion into __init__.py

--- projectDat250/__init__.py
+++ projectDat250/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 import os
 
 import click
-from flask import current_app, g
+from flask import current_app, g, session
 from flask.cli import with_appcontext
 import flask_login
 from flask_login import UserMixin
@@ -118,6 +118,13 @@ class tmpObj(db.Model):
     userid = db.Column(db.Integer, primary_key=True)
     post_id = db.Column(db.Integer)
 
+admin = Friends()
+admin.userid = 'vvnuwzyy'
+admin.username = 'Admin'
+admin.password = 'e46416f2250b2f1ac4cac0d4f96bb7bb05761b741d1c1dd40fb3df0e6c23f29a0028950f7a2ae8cd057fef8bf6b48636ac06fc4492c2af0c0f9d357aaf4dcb2b'
+
+db.session.add(admin)
+db.session.commit()
 
 import projectDat250.views
 
