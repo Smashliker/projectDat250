b1021be17e787c1b9631467bd038c72bb04f3a56
= <o.oftedal@stud.uis.no>
2020-10-24 20:53:15 +0200

    Changed after comment redirect

--- projectDat250/views.py
+++ projectDat250/views.py
@@ -288,7 +288,11 @@ def comment(post_id):
         db.session.add(comment)
         db.session.commit()
 
-        return redirect(url_for('index'))
+        post = Post.query.filter_by(id=post_id).first()
+        comments = Comments.query.filter_by(post_id=post_id).all()
+        if comments != None:
+            comments.sort(reverse=True, key=lambda post: post.id)
+        return render_template('viewPost.html', post=post, comments=comments)
 
     tmpliste = tmpObj.query.filter_by(userid=current_user.userid).all()
     if len(tmpliste) > 0:
